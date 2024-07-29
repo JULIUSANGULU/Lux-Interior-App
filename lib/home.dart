6cc1c8c08e4ca61f95cfb4de923f10b14cda9b41
JULIUSANGULU <74071966+JULIUSANGULU@users.noreply.github.com>
2024-07-29 11:42:47 +0100

    Homepage Appbar

--- lib/home.dart
+++ lib/home.dart
@@ -14,27 +14,99 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: const Color(0xFFEFF0F0),
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Padding(
-          padding: const EdgeInsets.all(9.0),
-          child: Container(
-            width: 10,
-            height: 10,
-            decoration: ShapeDecoration(
-              color: const Color(0xFFB9CDD2),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+        backgroundColor: const Color(0xFFEFF0F0),
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              SvgPicture.asset(
+                'assets/images/carbonlocation.svg',
               ),
-            ),
-            child: (IconButton(
-              icon: SvgPicture.asset("assets/images/chevronarrow2.svg"),
-              onPressed: () {
-                Navigator.pushNamed(context, '/signup');
-              },
-            )),
+              const SizedBox(width: 8),
+              const Expanded(
+                child: Text(
+                  'Abuja, Nigeria',
+                  style: TextStyle(fontSize: 16),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ],
           ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 24, 0),
+            child: Row(
+              children: [
+                SvgPicture.asset('assets/images/reminder.svg'),
+                const SizedBox(
+                  width: 8,
+                ),
+                SvgPicture.asset('assets/images/cartoutline.svg'),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color(0xFFd9d9d9), // Custom fill color
+                hintText: 'Search',
+                hintStyle: const TextStyle(
+                  color: Color(0xFF828383),
+                  fontSize: 16,
+                  fontFamily: 'Nunito',
+                ),
+                prefixIcon: Padding(
+                  padding:
+                      const EdgeInsets.all(8.0), // Adjust padding as needed
+                  child: SvgPicture.asset(
+                    "assets/images/searchicon.svg", // Your search icon asset path
+                    width: 20,
+                    height: 20,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                suffixIcon: Padding(
+                  padding:
+                      const EdgeInsets.all(8.0), // Adjust padding as needed
+                  child: SvgPicture.asset(
+                    "assets/images/sliderfilter.svg", // Your search icon asset path
+                    width: 20,
+                    height: 20,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius:
+                      BorderRadius.circular(10.0), // Custom border radius
+                  borderSide: BorderSide.none, // Remove the border
+                ),
+                contentPadding: const EdgeInsets.symmetric(
+                    vertical: 10.0), // Adjust padding inside the text field
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(24, 0, 0, 0),
+            child: Text(
+              "Special Offers",
+              style: TextStyle(
+                color: Color(0xFF2A2A2A),
+                fontSize: 16,
+                fontFamily: 'Nunito',
+                fontWeight: FontWeight.w600,
+                height: 0.09,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
